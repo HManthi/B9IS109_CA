2ed5ea8b82d03f84828a02449b7d890a857b057f
HManthi <32368168+HManthi@users.noreply.github.com>
2024-03-10 23:30:21 +0000

    book store
    project creation and loging page and home page

---
+++ book_Store/client/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../css/Home.css'
+
+const Home = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-content'>
+            <h1 className='hero-text'>Reader's Haven</h1>
+                <p className='hero-description'>
+                Discover and buy books online. Wide selection, author info, customer reviews, and fast delivery. Support local bookstores and authors.
+                </p>
+        </div>
+        <div className='hero-image'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
